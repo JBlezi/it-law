5960b588aaf62ea8c01805206be896a3380f0a88
Julius Blezinger <jgoldappel@gmail.com>
2023-11-24 15:06:12 +0100

    implements calculated distance from hero to Articles

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import EuKommission from './images/Eu-Kommission-PS3.png';
 import Button from './button';
 import Article from './Article';
@@ -11,6 +11,16 @@ import { fetchBlogPosts, fetchSocial } from './contentful';
 const HomePage = () => {
   const [posts, setPosts] = useState([]);
   const [socials, setSocials] = useState([]);
+  const titleRef = useRef(null); // Create a ref for the title element
+  const [titleHeight, setTitleHeight] = useState(0);
+
+  useEffect(() => {
+    if (titleRef.current) {
+      const height = titleRef.current.offsetHeight; // Get the height of the title element
+      setTitleHeight(height); // Set the state with the height
+    }
+  }, [posts]); // Depend on posts so it recalculates when posts data changes
+
 
 
   useEffect(() => {
@@ -52,10 +62,10 @@ const HomePage = () => {
 
   return (
     <div>
-      <div style={backgroundStyle} className='h-[90vh] relative'>
+      <div style={backgroundStyle} className='h-[80vh] relative'>
         <div className='absolute inset-0 bg-black bg-opacity-50'></div>
         <div className='mx-8 relative pt-12'>
-          <h1 className='text-4xl font-bold mb-4'>{ posts.length > 0 ? posts[0].fields.title : ""}</h1>
+          <h1 className='text-4xl font-bold mb-4' ref={titleRef}>{ posts.length > 0 ? posts[0].fields.title : ""}</h1>
           <h2 className='text-xl font-medium mb-4'>by <span className='underline'>{ posts.length > 0 ? posts[0].fields.authors[0] : ""},</span> <span className='underline'>{ posts.length > 0 ? posts[0].fields.authors[1] : ""} </span>| { posts.length > 0 ? formatDate(posts[0].sys.createdAt) : ""}| {posts.length > 0 ? `${calculateReadingTime(posts[0].fields.content)} min read` : ""}</h2>
           <p className='text-2xl font-medium mb-8 line-clamp-3'>{ posts.length > 0 ? posts[0].fields.content : ""}</p>
           <Button color='main' link='/home' text='READ MORE'/>
@@ -65,7 +75,7 @@ const HomePage = () => {
           </div>
         </div>
       </div>
-      <div className='mt-64'>
+      <div style={{ marginTop: titleHeight + 100 }}>
         {posts.map(post => (
           <Article key={post.sys.id} header={post.fields.title} image={post.fields.image.fields.file.url} authors={post.fields.authors} date={formatDate(post.sys.createdAt)} reading_time={`${calculateReadingTime(post.fields.content)} min read`} content={post.fields.content}/>
         ))}
