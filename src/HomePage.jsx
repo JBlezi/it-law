22611a6ecd4cfdaa2500a2412362c93fe784301e
Julius Blezinger <jgoldappel@gmail.com>
2023-11-24 14:36:22 +0100

    implements calculated reading time and date

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -27,13 +27,19 @@ const HomePage = () => {
     getSocials();
   },[]);
 
-  const article1 = {
-    image: EuKommission,
-    header: 'The EU Commission’s proposals for PSD3 – transitional provisions, Art. 44, 45 PSD3',
-    authors: {first: 'Dr. Paul Schultess', second: 'Jonas Philipp'},
-    date: '21. September 2023',
-    reading_time: '8',
-    content: 'On 28 June 2023, the EU Commission presented proposals for a Payment Services Directive 3 (“PSD3“) and a…'
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are 0-indexed
+    const year = date.getFullYear();
+    return `${day}.${month}.${year}`;
+  };
+
+  const calculateReadingTime = (content) => {
+    const wordsPerMinute = 200; // Average reading speed
+    const textLength = content.split(/\s+/).length; // Split by whitespace and count
+    const readingTime = Math.ceil(textLength / wordsPerMinute);
+    return readingTime;
   };
 
 
@@ -49,9 +55,9 @@ const HomePage = () => {
       <div style={backgroundStyle} className='h-[90vh] relative'>
         <div className='absolute inset-0 bg-black bg-opacity-50'></div>
         <div className='mx-8 relative pt-12'>
-          <h1 className='text-4xl font-bold mb-4'>{article1.header}</h1>
-          <h2 className='text-xl font-medium mb-4'>by <span className='underline'>{article1.authors.first},</span> <span className='underline'>{article1.authors.second} </span>| {article1.date} | {article1.reading_time} min read</h2>
-          <p className='text-2xl font-medium mb-8'>{article1.content}</p>
+          <h1 className='text-4xl font-bold mb-4'>{ posts.length > 0 ? posts[0].fields.title : ""}</h1>
+          <h2 className='text-xl font-medium mb-4'>by <span className='underline'>{ posts.length > 0 ? posts[0].fields.authors[0] : ""},</span> <span className='underline'>{ posts.length > 0 ? posts[0].fields.authors[1] : ""} </span>| { posts.length > 0 ? formatDate(posts[0].sys.createdAt) : ""}| {posts.length > 0 ? `${calculateReadingTime(posts[0].fields.content)} min read` : ""}</h2>
+          <p className='text-2xl font-medium mb-8 line-clamp-3'>{ posts.length > 0 ? posts[0].fields.content : ""}</p>
           <Button color='main' link='/home' text='READ MORE'/>
           <div className='h-[28rem] bg-white p-8 my-8 rounded-lg shadow-lg'>
             <h2 className='text-grey underline'>NEWS</h2>
@@ -61,7 +67,7 @@ const HomePage = () => {
       </div>
       <div className='mt-64'>
         {posts.map(post => (
-          <Article key={post.sys.id} header={post.fields.title} image={post.fields.image.fields.file.url} authors={post.fields.authors} date={article1.date} reading_time={article1.reading_time} content={post.fields.content}/>
+          <Article key={post.sys.id} header={post.fields.title} image={post.fields.image.fields.file.url} authors={post.fields.authors} date={formatDate(post.sys.createdAt)} reading_time={`${calculateReadingTime(post.fields.content)} min read`} content={post.fields.content}/>
         ))}
       </div>
       <div className='mt-32 flex flex-wrap mx-8'>
