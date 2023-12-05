4e604b459df5e6cb78b5a7580eac3cd36f29c32f
Julius Blezinger <jgoldappel@gmail.com>
2023-12-05 13:44:24 +0100

    deploy settings

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,31 +1,9 @@
-const fetch = require('node-fetch');
-const Parser = require('rss-parser');
 
-const parser = new Parser();
-
-exports.handler = async (event, context) => {
-  try {
-    // Check if URL parameter is provided
-    const { url } = event.queryStringParameters;
-    if (!url) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ error: 'URL parameter is required' })
-      };
-    }
-
-    // Decode and parse the RSS feed
-    const feedUrl = decodeURIComponent(url);
-    const feed = await parser.parseURL(feedUrl);
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(feed)
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Internal Server Error' })
-    };
-  }
-};
+export const handler = async () => {
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			message: 'This is what will be returned!'
+		})
+	}
+}
